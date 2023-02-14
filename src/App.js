561b7ccb80b81bc416217ec26bbb84e250d15968
Phinjock Sherpa <phinjock@gmail.com>
2023-02-14 11:41:57 -0800

    create functionality in formlist onsubmit,onchange,id

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import TodoForm from "./components/TodoForm";
+// import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 const App = () => {
   return (
     <div className="todo">
-      <TodoForm />
+      <TodoList />
     </div>
   );
 };
