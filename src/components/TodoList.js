29d4f8b00c3b0921cf9700681207767a30a01d49
Phinjock Sherpa <phinjock@gmail.com>
2023-02-15 10:34:44 -0800

    updated regex to input todo list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,6 +3,11 @@ import TodoForm from "./TodoForm";
 
 const TodoList = () => {
   const [todos, setTodos] = useState([]);
+  const addTodo = todo =>{
+    if(!todo.text || /^\s*$.test(todo.text)){
+      return;
+    }
+  }
   return (
     <div>
       <h1>What's the Plan for Today</h1>
