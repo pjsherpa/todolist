545f51bf7d02141879ef1ab9067b7aa765db8537
Phinjock Sherpa <phinjock@gmail.com>
2023-02-15 12:35:15 -0800

    function to store data, error found not displaying

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import TodoForm from "./TodoForm";
+import Todo from "./Todo";
 
-const TodoList = () => {
+function TodoList() {
   const [todos, setTodos] = useState([]);
   const addTodo = (todo) => {
     if (!todo.text || /^\s*$/.test(todo.text)) {
@@ -11,12 +12,22 @@ const TodoList = () => {
     setTodos(newTodos);
     console.log(...todos);
   };
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
   return (
     <div>
       <h1>What's the Plan for Today</h1>
       <TodoForm onSubmit={addTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} />
     </div>
   );
-};
+}
 
 export default TodoList;
