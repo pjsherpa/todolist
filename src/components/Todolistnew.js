a036dbaeab35374510765adb8eb0e260e8b66fd6
Phinjock Sherpa <phinjock@gmail.com>
2023-03-01 15:14:06 -0800

    found and fixed error to present list

--- src/components/Todolistnew.js
+++ src/components/Todolistnew.js
@@ -3,34 +3,35 @@ import "./styles.css";
 
 export default function App() {
   const initialState = [
-    "Go shopping",
-    "Do laundry!",
+    "Go Shopping",
+    "Do Laundry",
     "Read a book",
-    "Go to Gym",
+    "Go to gym",
   ];
+
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState(initialState);
 
-  const handleChange = (e) => {
-    setTodo(e.target.value);
-  };
+  const handlechange = (e) => setTodo(e.target.value);
+
   const addTodo = () => {
-    const newTodos = todos.concat(todo);
-    setTodos(newTodos);
+    const newTodo = todos.concat(todo);
+    setTodos(newTodo);
   };
-  const listTodos = () => {
-    return todos.map((itm) => <li>{itm}</li>);
-  };
+  const todosList = () => initialState.map((itm) => <li>{itm}</li>);
   return (
-    <div>
-      <div className="App">
-        <h1>TODOS</h1>
-        <div>
-          <input placeholder="Add todo" onChange={handleChange} />
-          <button onClick={addTodo}>Add Todo</button>
-        </div>
-        <ol>{listTodos()}</ol>
+    <div className="App">
+      <h1>Todos</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="
+        Add todo"
+          onChange={handlechange}
+        />
+        <button onClick={addTodo}>Add Todo</button>
       </div>
+      <ol>{todosList()}</ol>
     </div>
   );
 }
