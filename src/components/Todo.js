6869faa000be95fbe4ada9922bc2ec59c11723e4
Phinjock Sherpa <phinjock@gmail.com>
2023-04-14 12:11:40 -0700

    updated todoList for personal use

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,22 +1,7 @@
-import React, { useState } from "react";
-import TodoForm from "./TodoForm";
+import React from "react";
 
 const Todo = () => {
-  const [edit, setEdit] = useState({
-    id: null,
-    value: "",
-  });
-
-  return todos.map((todo, index) => (
-    <div
-      className={todo.isComplete ? "todo-row complete" : "todo-row"}
-      key={index}
-    >
-      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-        {todo.text}
-      </div>
-    </div>
-  ));
+  return <div></div>;
 };
 
 export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,33 +1,68 @@
 import React, { useState } from "react";
-import TodoForm from "./TodoForm";
-import Todo from "./Todo";
 
-function TodoList() {
-  const [todos, setTodos] = useState([]);
-  const addTodo = (todo) => {
-    if (!todo.text || /^\s*$/.test(todo.text)) {
-      return;
-    }
-    const newTodos = [todo, ...todos];
-    setTodos(newTodos);
-    console.log(...todos);
+const TodoList = () => {
+  const [todoInput, setTodoInput] = useState("");
+  const [list, setList] = useState([
+    { id: 1, text: "new" },
+    { id: 2, text: "new 2" },
+  ]);
+
+  const [editInput, setEditInput] = useState();
+  const [editList, setEditList] = useState(null);
+
+  const handleChange = (e) => setTodoInput(e.target.value);
+
+  const addTodo = () => {
+    const newTodo = [...list, { id: todoInput, text: todoInput }];
+    setList(newTodo);
+    setTodoInput("");
   };
-  const completeTodo = (id) => {
-    let updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        todo.isComplete = !todo.isComplete;
+
+  const removeTodo = (id) => {
+    const deleteTodo = list.filter((list) => id !== list.id);
+    setList(deleteTodo);
+  };
+
+  const editTodo = (text) => {
+    const edits = list.map((item) => {
+      if (text === item.text) {
+        item.text = editInput;
       }
-      return todo;
+      return item;
     });
-    setTodos(updatedTodos);
+    setList(edits);
+    setEditList(null);
+    setEditInput("");
   };
+
+  const displayList = () =>
+    list.map((item) => (
+      <li>
+        {editList === item.text ? (
+          <div>
+            <input
+              onChange={(e) => setEditInput(e.target.value)}
+              value={editInput}
+            />
+            <button onClick={() => editTodo(item.text)}>Save Edit</button>
+          </div>
+        ) : (
+          item.text
+        )}
+        <button onClick={() => removeTodo(item.id)}>Remove</button>
+        <button onClick={() => setEditList(item.id)}>Edit</button>
+      </li>
+    ));
   return (
     <div>
-      <h1>What's the Plan for Today</h1>
-      <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} />
+      <h1>Todo List App</h1>
+      <div>
+        <input value={todoInput} onChange={handleChange} />
+        <button onClick={addTodo}>Save</button>
+      </div>
+      <ol>{displayList()}</ol>
     </div>
   );
-}
+};
 
 export default TodoList;
